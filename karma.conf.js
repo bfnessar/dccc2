ac99f15b74bf46cfccaefaa61f22559aec1b9203
bfnessar <bfnessar@email.wm.edu>
2017-03-10 18:58:18 -0500

    Success? I needed to change package.json.scripts.start from './app' to './'

--- karma.conf.js
+++ karma.conf.js
@@ -2,14 +2,14 @@
 module.exports = function(config) {
   config.set({
 
-    basePath: './app',
+    basePath: './',
 
     files: [
       'bower_components/angular/angular.js',
       'bower_components/angular-route/angular-route.js',
       'bower_components/angular-mocks/angular-mocks.js',
-      'components/**/*.js',
-      'view*/**/*.js'
+      '/components/**/*.js',
+      '/view*/**/*.js'
     ],
 
     autoWatch: true,
